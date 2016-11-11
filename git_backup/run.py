57a6710b020adf66c5ebb70c434cb4c1f5bea82e
Daniel Ancuta <whisller@gmail.com>
2016-11-11 14:13:20 +0000

    Two main changes: - add prefix `github` and `bitbucket` for project in directory - refresh projects using `Popen`

--- git_backup/run.py
+++ git_backup/run.py
@@ -1,13 +1,15 @@
 import argparse
 import configparser
 import os
-
 from abc import ABCMeta, abstractmethod
-from subprocess import call
+from subprocess import call, Popen
 from github import Github
 from pybitbucket.auth import BasicAuthenticator
 from pybitbucket.bitbucket import Client
 from pybitbucket.repository import Repository
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+CACHED_REPOSITORIES = os.path.join(DIR_PATH, '.repositories')
 
 
 class GitIntegration(metaclass=ABCMeta):
@@ -16,21 +18,30 @@ class GitIntegration(metaclass=ABCMeta):
         pass
 
     def refresh_list_of_projects(self, path):
+        file = open(CACHED_REPOSITORIES, 'a')
+
         for info in self.get_list_of_repos():
-            repo_path = path + '/' + info['name']
+            repo_path = os.path.join(path, info['name'])
+            file.writelines(repo_path+"\n")
 
             if os.path.isdir(repo_path) is False:
                 os.makedirs(repo_path)
 
                 call(['git', 'init'], cwd=repo_path)
                 call(['git', 'remote', 'add', 'origin', info['url']], cwd=repo_path)
+        file.close()
 
-    def refresh_projects(self, path):
-        for directory in [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]:
-            working_dir = path + '/' + directory
+    def refresh_projects(self):
+        if os.path.isfile(CACHED_REPOSITORIES) is False:
+            print('Please refresh list of projects first by running script with flag "--refresh-list-of-projects"')
+        else:
+            with open(CACHED_REPOSITORIES) as f:
+                for line in f:
+                    path = line.strip()
 
-            call(['git', 'remote', 'prune', 'origin'], cwd=working_dir)
-            call(['git', 'remote', 'update'], cwd=working_dir)
+                    if os.path.isdir(path):
+                        Popen(['git', 'remote', 'prune', 'origin'], cwd=path)
+                        Popen(['git', 'remote', 'update'], cwd=path)
 
 
 class GithubIntegration(GitIntegration):
@@ -41,7 +52,7 @@ class GithubIntegration(GitIntegration):
         result = []
 
         for repo in self._api.get_user().get_repos():
-            info = {'name': repo.name,
+            info = {'name': os.path.join('github', repo.full_name),
                     'url': repo.ssh_url}
             result.append(info)
 
@@ -58,7 +69,7 @@ class BitbucketIntegration(GitIntegration):
         result = []
 
         for repository in Repository.find_repositories_by_owner_and_role(client=self._api):
-            info = {'name': repository.name,
+            info = {'name': os.path.join('bitbucket', repository.full_name),
                     'url': repository.links['clone'][1]['href']}
             result.append(info)
 
@@ -91,22 +102,30 @@ class GitBackupRunner:
             integrations.append(bitbucket)
 
         if args.refresh_list_of_projects:
+            try:
+                os.remove(CACHED_REPOSITORIES)
+            except OSError:
+                pass
+
             for integration in integrations:
                 integration.refresh_list_of_projects(backup_path)
 
         if args.refresh_projects:
-            for integration in integrations:
-                integration.refresh_projects(backup_path)
+            if os.path.isfile(CACHED_REPOSITORIES) is False:
+                print('Please refresh list of projects first by running script with flag "--refresh-list-of-projects"')
+            else:
+                for integration in integrations:
+                    integration.refresh_projects()
 
     def run(self):
         parser = argparse.ArgumentParser(description='Git backup.')
         parser.add_argument('--refresh-list-of-projects',
-                            help='Loads list of projects from remote servers.',
+                            help='Loads list of projects from remote servers (git init).',
                             nargs='?',
                             default=False,
                             const=True)
         parser.add_argument('--refresh-projects',
-                            help='Loads content of projects from remote servers.',
+                            help='Loads content of projects from remote servers (git update).',
                             nargs='?',
                             default=False,
                             const=True)
