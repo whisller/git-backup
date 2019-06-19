6db04925b5b0346644b0a935cdced594f7e48b44
whisller <whisller@gmail.com>
2019-06-19 21:28:26 +0100

    Some changes.
    Some changes ;)

--- README.md
+++ README.md
@@ -1,43 +1,19 @@
 # Git backup.
 
-Backups all of your remote git repositories ([GitHub](https://github.com/) / [Bitbucket](https://bitbucket.org/)) locally.
+Backups all of your GitHub remote git repositories locally.
 
-
-### What do I need?
+## What do I need?
 - Python 3+
 
+## How to run it?
+1. Set `GITHUB_TOKEN` env variable with your github token
 
-### How to run it?
-
-#### Install by hand
+2. Execute script with path to where repositories should be saved
 ```
-$ git clone https://github.com/whisller/git-backup.git
+git-backup --path=~/Documents/backup
 ```
 
-#### Install with pypi
+## Install with pypi
 ```
 $ pip install git-repo-backup
 ```
-
-#### Configure it
-1. Create configuration file, you can see example inside of `git_backup/config.cfg.dist`
-2. Set your Github/Bitbucket access token and path to where repositories will be stored
-
-#### Install dependencies
-```
-$ python setup.py install
-```
-
-#### Execute it
-```
-$ python git_backup/run.py
-```
-or
-```
-$ git_backup_run
-```
-
-### FAQ
-Q: How can I export it to Google Drive/Dropbox/etc.
-
-A: Just set `BACKUP_PATH` from `config.py` to a path that is synchronised with your cloud storage
---
+++ git_repo_backup/backup.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+import click
+from git import Repo, InvalidGitRepositoryError, NoSuchPathError
+from github import Github
+from tqdm import tqdm
+
+
+@click.command()
+@click.option("--path", required=True, help="Path to where repositories will be saved.")
+def cli(path):
+    repos = github_integration(path)
+
+    for repo in tqdm(repos, bar_format="Processing github repositories: {n}"):
+        local_repository_path = os.path.join(path, "github", repo["name"])
+
+        try:
+            repo = Repo(local_repository_path)
+        except (InvalidGitRepositoryError, NoSuchPathError):
+            if not os.path.isdir(local_repository_path):
+                os.makedirs(local_repository_path)
+            repo = Repo.clone_from(f"https://github.com/{repo['name']}.git", local_repository_path)
+
+        for remote in repo.remotes:
+            remote.fetch()
+            remote.pull()
+
+        repo.submodule_update()
+
+
+def github_integration(path):
+    _validate({"GITHUB_TOKEN"})
+
+    github = Github(os.environ["GITHUB_TOKEN"])
+    github_repos = github.get_user().get_repos()
+
+    for r in github_repos:
+        yield {'name': r.full_name}
+
+
+def _validate(required_envs):
+    missing_env = required_envs - os.environ.keys()
+    if missing_env:
+        click.secho(f'Missing env variables: "{",".join(missing_env)}".', fg="red")
+        sys.exit()
+
+
+if __name__ == '__main__':
+    cli()
